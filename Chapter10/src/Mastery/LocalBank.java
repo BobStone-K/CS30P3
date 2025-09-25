50b42622b88aae1a571218074352cb5231c19233
38262004 <38262004@L815WD220011.edu.cbe.ab.ca>
2025-09-25 15:36:01 -0600

    chiz

--- Chapter10/src/Mastery/LocalBank.java
+++ Chapter10/src/Mastery/LocalBank.java
@@ -12,13 +12,15 @@ import javax.swing.JTextField;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class LocalBank {
-
+	Bank lcBank = new Bank();
 	private JFrame frame;
 	private JTextField accNumText;
 	private JTextField amountofText;
-	private JTextField fNameField;
+	private JTextField fNameText;
 	private JTextField lNameText;
 	private JTextField beginningBalText;
 
@@ -65,7 +67,7 @@ public class LocalBank {
 		panel.add(selectAction);
 		
 		JComboBox actionBox = new JComboBox();
-		actionBox.setModel(new DefaultComboBoxModel(new String[] {"Add an account", "Remove an account", "Deposit", "Withdrawl", "Check Balance"}));
+		actionBox.setModel(new DefaultComboBoxModel(new String[] {"Withdrawl", "Add an account", "Remove an account", "Deposit", "Check Balance"}));
 		actionBox.setBounds(21, 44, 383, 22);
 		panel.add(actionBox);
 		
@@ -74,22 +76,22 @@ public class LocalBank {
 		infoWarner.setBounds(21, 77, 237, 22);
 		panel.add(infoWarner);
 		
-		JLabel AccountNumberDis = new JLabel("Account number:");
-		AccountNumberDis.setForeground(new Color(225, 0, 113));
-		AccountNumberDis.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		AccountNumberDis.setBounds(21, 102, 169, 20);
-		panel.add(AccountNumberDis);
+		JLabel accNumberDis = new JLabel("Account number:");
+		accNumberDis.setForeground(new Color(255, 0, 0));
+		accNumberDis.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		accNumberDis.setBounds(21, 102, 169, 20);
+		panel.add(accNumberDis);
 		
 		accNumText = new JTextField();
 		accNumText.setBounds(20, 125, 384, 20);
 		panel.add(accNumText);
 		accNumText.setColumns(10);
 		
-		JLabel AmountOfDis = new JLabel("Amount of deposit/withdrawl:");
-		AmountOfDis.setForeground(new Color(225, 0, 113));
-		AmountOfDis.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		AmountOfDis.setBounds(21, 156, 186, 20);
-		panel.add(AmountOfDis);
+		JLabel amountOfDis = new JLabel("Amount of deposit/withdrawl:");
+		amountOfDis.setForeground(new Color(255, 0, 0));
+		amountOfDis.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		amountOfDis.setBounds(21, 156, 186, 20);
+		panel.add(amountOfDis);
 		
 		amountofText = new JTextField();
 		amountofText.setBounds(21, 179, 383, 20);
@@ -101,10 +103,10 @@ public class LocalBank {
 		fNameDis.setBounds(21, 210, 383, 14);
 		panel.add(fNameDis);
 		
-		fNameField = new JTextField();
-		fNameField.setColumns(10);
-		fNameField.setBounds(21, 226, 383, 20);
-		panel.add(fNameField);
+		fNameText = new JTextField();
+		fNameText.setColumns(10);
+		fNameText.setBounds(21, 226, 383, 20);
+		panel.add(fNameText);
 		
 		JLabel lNameDis = new JLabel("Last Name:");
 		lNameDis.setFont(new Font("Tahoma", Font.PLAIN, 13));
@@ -131,7 +133,83 @@ public class LocalBank {
 		resultDis.setBounds(21, 351, 383, 51);
 		panel.add(resultDis);
 		
+		
+		actionBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			String choice = (String) actionBox.getSelectedItem();
+			if(choice.equals("Withdrawl")) {
+				accNumberDis.setForeground(Color.red);
+				amountOfDis.setForeground(Color.red);
+				fNameDis.setForeground(Color.black);
+				lNameDis.setForeground(Color.black);
+				beginningBalDis.setForeground(Color.black);
+			}else if(choice.equals("Add an account")) {
+				accNumberDis.setForeground(Color.red);
+				amountOfDis.setForeground(Color.black);
+				fNameDis.setForeground(Color.red);
+				lNameDis.setForeground(Color.red);
+				beginningBalDis.setForeground(Color.red);
+				
+			}else if(choice.equals("Remove an account")) {
+				accNumberDis.setForeground(Color.red);
+				amountOfDis.setForeground(Color.black);
+				fNameDis.setForeground(Color.black);
+				lNameDis.setForeground(Color.black);
+				beginningBalDis.setForeground(Color.black);
+				
+			}else if(choice.equals("Deposit")) {
+				accNumberDis.setForeground(Color.red);
+				amountOfDis.setForeground(Color.red);
+				fNameDis.setForeground(Color.black);
+				lNameDis.setForeground(Color.black);
+				beginningBalDis.setForeground(Color.black);
+			}else if(choice.equals("Check Balance")) {
+				accNumberDis.setForeground(Color.red);
+				amountOfDis.setForeground(Color.black);
+				fNameDis.setForeground(Color.black);
+				lNameDis.setForeground(Color.black);
+				beginningBalDis.setForeground(Color.black);
+				
+			}
+			
+			
+			
+			
+			
+			
+			}
+		});
 		JButton transactionB = new JButton("Process Transaction");
+		transactionB.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String choice = (String) actionBox.getSelectedItem();
+//				String accId = accNumText.getText();
+//				double amountNum = Double.parseDouble(amountofText.getText());
+				String lName = lNameText.getText();
+//				String fName = fNameText.getText();
+//				double ogBal = Double.parseDouble(beginningBalText.getText());
+//				System.out.println(amountNum);
+				System.out.println(lName);
+				if(choice.equals("Withdrawl")) {
+					if(accNumText.getText().equals("") || (amountofText.getText().equals(""))) {
+						resultDis.setText("Required Fields are not filled in");
+					}
+					lcBank.transaction(2, accNumText.getText(), Double.parseDouble(amountofText.getText()));
+				}else if(choice.equals("Add an account")) {
+		
+					
+				}else if(choice.equals("Remove an account")) {
+			
+					
+				}else if(choice.equals("Deposit")) {
+		
+					beginningBalDis.setForeground(Color.black);
+				}else if(choice.equals("Check Balance")) {
+				
+					
+				}
+			}
+		});
 		transactionB.setBounds(21, 413, 145, 29);
 		panel.add(transactionB);
 	}
