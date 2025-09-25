adfcdcd7bd47d199a73fd47378a66411d40eb5b8
youdi <youdi@192.168.1.153>
2025-09-24 23:36:46 -0600

    chiz

---
+++ Chapter10/src/Mastery/LocalBank.java
@@ -0,0 +1,138 @@
+package Mastery;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+
+public class LocalBank {
+
+	private JFrame frame;
+	private JTextField accNumText;
+	private JTextField amountofText;
+	private JTextField fNameField;
+	private JTextField lNameText;
+	private JTextField beginningBalText;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LocalBank window = new LocalBank();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LocalBank() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 536);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 414, 475);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel selectAction = new JLabel("Select An Action: ");
+		selectAction.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		selectAction.setBounds(21, 11, 169, 22);
+		panel.add(selectAction);
+		
+		JComboBox actionBox = new JComboBox();
+		actionBox.setModel(new DefaultComboBoxModel(new String[] {"Add an account", "Remove an account", "Deposit", "Withdrawl", "Check Balance"}));
+		actionBox.setBounds(21, 44, 383, 22);
+		panel.add(actionBox);
+		
+		JLabel infoWarner = new JLabel("Complete Information in Red");
+		infoWarner.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		infoWarner.setBounds(21, 77, 237, 22);
+		panel.add(infoWarner);
+		
+		JLabel AccountNumberDis = new JLabel("Account number:");
+		AccountNumberDis.setForeground(new Color(225, 0, 113));
+		AccountNumberDis.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		AccountNumberDis.setBounds(21, 102, 169, 20);
+		panel.add(AccountNumberDis);
+		
+		accNumText = new JTextField();
+		accNumText.setBounds(20, 125, 384, 20);
+		panel.add(accNumText);
+		accNumText.setColumns(10);
+		
+		JLabel AmountOfDis = new JLabel("Amount of deposit/withdrawl:");
+		AmountOfDis.setForeground(new Color(225, 0, 113));
+		AmountOfDis.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		AmountOfDis.setBounds(21, 156, 186, 20);
+		panel.add(AmountOfDis);
+		
+		amountofText = new JTextField();
+		amountofText.setBounds(21, 179, 383, 20);
+		panel.add(amountofText);
+		amountofText.setColumns(10);
+		
+		JLabel fNameDis = new JLabel("First Name:");
+		fNameDis.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		fNameDis.setBounds(21, 210, 383, 14);
+		panel.add(fNameDis);
+		
+		fNameField = new JTextField();
+		fNameField.setColumns(10);
+		fNameField.setBounds(21, 226, 383, 20);
+		panel.add(fNameField);
+		
+		JLabel lNameDis = new JLabel("Last Name:");
+		lNameDis.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lNameDis.setBounds(21, 257, 383, 14);
+		panel.add(lNameDis);
+		
+		lNameText = new JTextField();
+		lNameText.setColumns(10);
+		lNameText.setBounds(21, 273, 383, 20);
+		panel.add(lNameText);
+		
+		JLabel beginningBalDis = new JLabel("Beginng Balance");
+		beginningBalDis.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		beginningBalDis.setBounds(21, 304, 383, 22);
+		panel.add(beginningBalDis);
+		
+		beginningBalText = new JTextField();
+		beginningBalText.setColumns(10);
+		beginningBalText.setBounds(21, 324, 383, 20);
+		panel.add(beginningBalText);
+		
+		JLabel resultDis = new JLabel("");
+		resultDis.setHorizontalAlignment(SwingConstants.CENTER);
+		resultDis.setBounds(21, 351, 383, 51);
+		panel.add(resultDis);
+		
+		JButton transactionB = new JButton("Process Transaction");
+		transactionB.setBounds(21, 413, 145, 29);
+		panel.add(transactionB);
+	}
+}
