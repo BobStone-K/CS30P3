8866af2c9093dedf59b7e0c9af9d8e34a254e846
38262004 <38262004@L815WD220011.edu.cbe.ab.ca>
2025-11-18 14:38:55 -0700

    waddup

---
+++ Chapter11/src/Skillbuilders/Assignment.java
@@ -0,0 +1,43 @@
+package Skillbuilders;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Assignment {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Assignment window = new Assignment();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Assignment() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
