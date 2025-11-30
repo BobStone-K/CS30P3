e029a7ca45543ba92161d57e3810b8f2154c0ea4
youdisa <youdisa@192.168.1.250>
2025-11-30 15:16:23 -0700

    hi

--- Chapter11/src/Skillbuilders/Roster.java
+++ Chapter11/src/Skillbuilders/Roster.java
@@ -17,7 +17,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 
-public class Roster {
+public class Roster extends JFrame{
 
 	private JFrame frame;
 	private JTextField fileNameField;
