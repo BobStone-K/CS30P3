70b5e9b1e661c866c45cbbdca1d0a5cc304aa4b3
38262004 <38262004@L815WD220011.edu.cbe.ab.ca>
2026-01-07 13:25:14 -0700

    hi

--- Chapter13/src/Skillbuilders/LinkedListTester.java
+++ Chapter13/src/Skillbuilders/LinkedListTester.java
@@ -3,8 +3,25 @@ package Skillbuilders;
 public class LinkedListTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		LinkedList list = new LinkedList();
+		
+		list.addAtFront("Youdis");
+		list.addAtFront("Richard");
+		list.addAtFront("Rasheed");
+		list.addAtFront("Usman");
+		
+		
+		list.addAtEnd("Crescent");
+		list.addAtEnd("Blue");
+		
+		System.out.println(list);
+		
+		System.out.println("List size: " + list.size() + "Items.");
+		
+		list.makeEmpty();
+		
+		System.out.println("List size: " + list.size() + "Items.");
 	}
 
 }
